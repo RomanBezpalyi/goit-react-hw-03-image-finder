98fa88f1d0660727082ca42d7ef551a23f5e7053
Roman Bezpalyi <romabespaly@gmail.com>
2019-07-26 09:59:23 +0300

    Updates

---
+++ precache-manifest.c6b2471dbc0881e4f4bc9edf74680e0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb959da83b5ddce3bcc4b6fb0a4f4fc",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "6dff38587bd526da626f",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.a420e9b0.chunk.css"
+  },
+  {
+    "revision": "56284749e4cb04cedd07",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.afda7b3a.chunk.js"
+  },
+  {
+    "revision": "6dff38587bd526da626f",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.e5bf654a.chunk.js"
+  },
+  {
+    "revision": "052338c15f0cfa810fb3",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.a33a0e9d.js"
+  }
+]);
